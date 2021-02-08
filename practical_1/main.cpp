4c6d9e940163b063a99f5f62cead3e251c865151
Mihail Benev <mihail.benev99@gmail.com>
2021-02-09 00:20:18 +0200

    Pong Done

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -15,6 +15,13 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 
+bool ai = true;
+Font font;
+Text text;
+int scoreLeft = 0;
+int scoreRight = 0;
+
+
 CircleShape ball;
 RectangleShape paddles[2];
 Vector2f ballVelocity;
@@ -29,6 +36,13 @@ void Load() {
 	// Set size and origin of ball
 	ball.setRadius(ballRadius - 3);
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
+
+	// Load font-face from res dir
+	font.loadFromFile("res/fonts/BigShouldersDisplay-Regular.ttf");
+	// Set text element to use font
+	text.setFont(font);
+	// set the character size to 24 pixels
+	text.setCharacterSize(40);
 }
 
 void Reset() {
@@ -38,6 +52,27 @@ void Reset() {
 	// reset Ball Position
 	ball.setPosition(gameWidth / 2, gameHeight / 2);
 	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+
+	// Update Score Text
+	text.setString(to_string(scoreLeft) + "		" + to_string(scoreRight));
+	// Keep Score Text Centered
+	text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
+}
+
+void AI(RectangleShape& paddle, float dt)
+{
+	float direction = 0.0f;
+
+	if (ball.getPosition().y > paddle.getPosition().y)
+	{
+		direction++;
+	}
+	else if (ball.getPosition().y < paddle.getPosition().y)
+	{
+		direction--;
+	}
+
+	paddle.move(0, direction * paddleSpeed * dt);
 }
 
 void Update(RenderWindow& window) {
@@ -73,8 +108,26 @@ void Update(RenderWindow& window) {
 	if (Keyboard::isKeyPressed(controls[3])) {
 		directionRight++;
 	}
-	paddles[0].move(0, directionLeft * paddleSpeed * dt);
-	paddles[1].move(0, directionRight * paddleSpeed * dt);
+	if (Keyboard::isKeyPressed(Keyboard::LShift))
+	{
+		if (ai)
+		{
+			ai = false;
+		}
+		else
+			ai = true;
+	}
+
+	if (!ai)
+	{
+		paddles[0].move(0, directionLeft * paddleSpeed * dt);
+		paddles[1].move(0, directionRight * paddleSpeed * dt);
+	}
+	else if (ai)
+	{
+		paddles[0].move(0, directionLeft * paddleSpeed * dt);
+		AI(paddles[1],dt);
+	}
 
 	ball.move(ballVelocity * dt);
 	// check ball collision
@@ -95,11 +148,13 @@ void Update(RenderWindow& window) {
 	else if (bx > gameWidth) {
 		// right wall
 		server = false;
+		scoreLeft++;
 		Reset();
 	}
 	else if (bx < 0) {
 		// left wall
 		server = true;
+		scoreRight++;
 		Reset();
 	}
 	else if (
@@ -149,6 +204,7 @@ void Render(RenderWindow& window) {
 	window.draw(paddles[0]);
 	window.draw(paddles[1]);
 	window.draw(ball);
+	window.draw(text);
 }
 
 int main() {
